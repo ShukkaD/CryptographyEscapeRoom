5030ba694bccf4d63cecf0ae17dad11397b1250a
ShukkaD <123425758+ShukkaD@users.noreply.github.com>
2024-06-26 20:28:00 -0400

    Update server.js

--- server.js
+++ server.js
@@ -20,10 +20,10 @@ app.use(bodyParser.json());
 app.use(express.static(path.join(__dirname, 'public')));
 const USERS_FILE = path.join(__dirname, 'users.json');
 const stages = [
-  { stageNumber: 1, story: 'Haha! You have been tricked by us, the hackers. To restart the power plant, you must get past the intense security measures set by us to keep you out. These measures can range from easily encrypted codes, to extremely hard maths. \n \n You must figure out the first password by using the intercepted clue that was being communicated by the hackers that you eavesdropped on.', passcode: 'decryption' },
-  { stageNumber: 2, story: 'Well, wow, I guess you aren’t as bad as I thought at ciphers. Here\'s a clue: You must look around for a paper, here you will see that 8 is used for the second part. Decode this: 42/34/44/11/44/24/33/22', passcode: 'rotating' },
-  { stageNumber: 3, story: 'Wow, you\'re doing pretty good but you probably won\’t get past this one. Just look for a note like the one from the first part, and the cipher will be revealed to you. Decode this: vniiolauls', passcode: 'villainous' },
-  { stageNumber: 4, story: 'Alright, you have to stop cracking our codes. How did you even get here that fast! I can’t let you solve this one in time.  Decode this: Lvtlxk', passcode: 'potato' },
+  { stageNumber: 1, story: 'Haha! You have been tricked by us, the hackers. To restart the power plant, you must get past the intense security measures set by us to keep you out. These measures can range from easily encrypted codes, to extremely hard maths. \n \n You must figure out the first password by looking around and trying to find a paper with Rome.', passcode: 'eigthissecond' },
+  { stageNumber: 2, story: 'Well, wow, I guess you aren’t as bad as I thought at ciphers. Here\'s a clue: You must look around for a paper, here you will see that 8 is used for the second part. Decode this: 42/34/44/11/44/24/33/22', passcode: 'sixthisthird' },
+  { stageNumber: 3, story: 'Wow, you\'re doing pretty good but you probably won\’t get past this one. Just look for a note like the one from the first part, and the cipher will be revealed to you. Decode this: fut sfutorhi orh', passcode: 'fourth is fourth' },
+  { stageNumber: 4, story: 'Alright, you have to stop cracking our codes. How did you even get here that fast! I can’t let you solve this one in time.  Decode this: xryriyxsl (keyword: poke)', passcode: 'idontknow' },
   { stageNumber: 5, story: 'You got here! Wha-wha-what, impossible, but you\'ll never be able to make it past the final defense. Enter this url into the your browser(keep this tab open or you will lose your progress): \n https://bit.ly/jumpy', passcode: 'nothing'}
 ];
 
